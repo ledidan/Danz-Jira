05535bc2698246098d7d2c8a3cc609a795f662ce
ledidan <didan.mobe@gmail.com>
2022-07-27 00:39:31 +0700

    update code

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   style: {
+    screen: {
+      plugins: [require("module.scss"), require("autoprefixer")],
+    },
     postcss: {
       plugins: [require("tailwindcss"), require("autoprefixer")],
     },
